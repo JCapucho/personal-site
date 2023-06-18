479d9d3f312715971751a242182174dbaf24c07f
João Capucho <jcapucho7@gmail.com>
2023-06-18 23:41:24 +0100

    Add initial article theming

---
+++ tailwind.config.mjs
@@ -0,0 +1,77 @@
+import typographyPlugin from "@tailwindcss/typography";
+
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `hsl(var(${variable}))`;
+    }
+    return `hsl(var(${variable}) / ${opacityValue})`;
+  };
+}
+
+export default {
+  jit: true,
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bgColor: withOpacityValue("--theme-bg"),
+        textColor: withOpacityValue("--theme-text"),
+        accent: withOpacityValue("--theme-accent"),
+        accent2: withOpacityValue("--theme-accent2"),
+        quoteColor: withOpacityValue("--theme-quote"),
+      },
+      typography: ({ theme }) => ({
+        capucho: {
+          css: {
+            "--tw-prose-body": theme("colors.textColor"),
+            "--tw-prose-headings": theme("colors.accent2"),
+            "--tw-prose-links": theme("colors.textColor"),
+            "--tw-prose-bold": theme("colors.textColor"),
+            "--tw-prose-bullets": theme("colors.textColor"),
+            "--tw-prose-quotes": theme("colors.quoteColor"),
+            "--tw-prose-code": theme("colors.textColor"),
+            "--tw-prose-hr": theme("colors.textColor"),
+            "--tw-prose-th-borders": theme("colors.textColor"),
+          },
+        },
+        DEFAULT: {
+          css: {
+            a: {
+              "@apply no-underline": "",
+            },
+            strong: {
+              fontWeight: "700",
+            },
+            code: {
+              border: "1px dotted #666",
+              borderRadius: "2px",
+            },
+            blockquote: {
+              borderLeftWidth: "none",
+            },
+            hr: {
+              borderTopStyle: "dashed",
+            },
+            thead: {
+              borderBottomWidth: "none",
+            },
+            "thead th": {
+              fontWeight: "700",
+              borderBottom: "1px dashed #666",
+            },
+            "tbody tr": {
+              borderBottomWidth: "none",
+            },
+            tfoot: {
+              borderTop: "1px dashed #666",
+            },
+          },
+        },
+      }),
+    },
+  },
+
+  plugins: [typographyPlugin],
+};
